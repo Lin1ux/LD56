682c95a576ec0aed48883fe5e18d92f2ec6e32ad
shamotte <123325697+shamotte@users.noreply.github.com>
2024-10-05 15:05:07 +0200

    better bees

--- bee/fly_towards.gd
+++ bee/fly_towards.gd
@@ -1,11 +1,36 @@
 extends Node
 
+@onready var bee = $"../.."
+const REDIRECT_DISTANCE = 25
+const NEW_POSITION_DISTANCE = 100
 
+
+var final_position : Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	enter()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func enter():
+	set_new_destination()
+
+func update(delta : float):
+	if final_position.distance_squared_to(bee.position) < REDIRECT_DISTANCE:
+		print("to close to the coursor")
+		enter()
+	if bee.get_global_mouse_position().distance_squared_to(final_position) > NEW_POSITION_DISTANCE:
+		print("coursor moved")
+		enter()
+	
+	
+
+
+	
+
+
+
+func set_new_destination():
+	var offset = Vector2(randf_range(-1,1),randf_range(-1,1)).normalized()* 50
+	final_position = bee.get_global_mouse_position() + offset
+	bee.new_target(final_position)
+	
