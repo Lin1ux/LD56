5233d1f4da0d04f8fa3e02a830ed83df48bf2c3c
Liniux <153652735+Lin1ux@users.noreply.github.com>
2024-10-07 00:31:03 +0200

    bird

--- components/comp_state_machine.gd
+++ components/comp_state_machine.gd
@@ -1,6 +1,8 @@
 extends Node
 
+@export var state_machine_name : String
 @export var state : Node	##first state
+
 
 
 
@@ -12,7 +14,7 @@ func _process(delta):
 	
 	
 func change_state(new_state : String):
-	print("state: ",new_state)
+	print("(",state_machine_name,") state: ",new_state)
 	var new = get_node_or_null(new_state)
 	if new:
 		state.exit()
