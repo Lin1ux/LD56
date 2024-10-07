497ec2619eabbc3cd7306df15f9979fdc417c930
igorekkedziorek <126686402+igorekkedziorek@users.noreply.github.com>
2024-10-07 19:40:28 +0200

    some minor changes

---
+++ enemies/bigbird/bird.gd
@@ -0,0 +1,28 @@
+extends Enemy
+
+@export var bee_manager : Node				##Bee manager to pick target from
+@export var after_dash_range : float = 100	##how close unit have to be to change dash state
+@export var correction : float = 0.4		##how often dash have correction
+var target : BeeControler
+
+func _process(delta: float) -> void:
+	$CompHP.rotation = -$Sprite2D.global_rotation + PI/2
+
+func get_target():
+	return target
+	
+func get_new_target():
+	target = bee_manager.get_random_bee()
+	return target
+	
+func get_after_dash_range():
+	return after_dash_range
+	
+func after_dash_range_sqr():
+	return after_dash_range * after_dash_range
+
+func get_correction():
+	return correction
+
+func can_deal_damage():
+	return true
