8409b612402c30f38db2fab9e8b16431b6adcc4f
Zed-z <71879051+Zed-z@users.noreply.github.com>
2024-10-12 15:36:50 +0200

    Better wasp movement

--- enemies/wasp/wasp.gd
+++ enemies/wasp/wasp.gd
@@ -4,10 +4,13 @@ extends CharacterBody2D
 @export var speed : float = 20.0
 @export var hive : Node2D
 
+@export var zigzag_offset: int = 0
+
 func _physics_process(delta: float) -> void:
-	look_at(hive.global_position)
-	velocity.x = -sign(self.get_position().x - hive.global_position.x) * speed
-	velocity.y =  -sign(self.get_position().y - hive.global_position.y) * speed
+	velocity = (hive.global_position - self.get_position()).normalized() * speed
+	velocity += Vector2.UP.rotated(global_rotation) * sin(deg_to_rad(zigzag_offset)) * 50
+	zigzag_offset += 4
+	look_at(velocity)
 	move_and_slide()
 
 
