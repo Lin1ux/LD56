a9144603a04cbae5bf98c4a9bb8e73b6b97f07e5
Zed-z <71879051+Zed-z@users.noreply.github.com>
2024-11-25 13:02:52 +0100

    Improved help box

--- help_box/help_box.gd
+++ help_box/help_box.gd
@@ -1,11 +1,13 @@
+@tool
 extends CanvasLayer
 
 
 
 func _ready() -> void:
-	visible = false
-	$AnimationPlayer.play("pop_in")
-	get_tree().paused = true
+	if not Engine.is_editor_hint():
+		visible = false
+		$AnimationPlayer.play("pop_in")
+		get_tree().paused = true
 
 func set_title(text: String):
 	%Title.text = text
@@ -15,11 +17,17 @@ func set_description(text: String):
 	
 func set_icon(texture: CompressedTexture2D):
 	%Icon.texture = texture
+	%Icon.scale.x = floor(%SpriteContainer.size.x / %Icon.texture.get_width())
+	%Icon.scale.y = floor(%SpriteContainer.size.y / %Icon.texture.get_height())
 
 func _on_button_pressed() -> void:
 	$AnimationPlayer.play("pop_out")
 	get_tree().paused = false
 
+func _process(_delta: float) -> void:
+	if Engine.is_editor_hint():
+		%Icon.scale.x = floor(%SpriteContainer.size.x / %Icon.texture.get_width())
+		%Icon.scale.y = floor(%SpriteContainer.size.y / %Icon.texture.get_height())
 
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
